2dc6b5ff92361252d8ab8aa3163529b58a61f2e9
Nepal Singh <nepalsss007@gmail.com>
2023-09-16 16:15:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let boxes = document.getElementsByClassName("inner-box");
+let array = [[boxes[0] ,boxes[1] ,boxes[2],boxes[3]],
+[boxes[4] ,boxes[5] ,boxes[6],boxes[7]],
+[boxes[8] ,boxes[9] ,boxes[10],boxes[11]],
+[boxes[12] ,boxes[13] ,boxes[14],boxes[15]]];
