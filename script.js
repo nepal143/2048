ebc7562190b2a419411997adef673b0654d5adf0
Nepal Singh <nepalsss007@gmail.com>
2023-09-16 16:43:40 +0530

    second commit

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ let array = [[boxes[0] ,boxes[1] ,boxes[2],boxes[3]],
 [boxes[4] ,boxes[5] ,boxes[6],boxes[7]],
 [boxes[8] ,boxes[9] ,boxes[10],boxes[11]],
 [boxes[12] ,boxes[13] ,boxes[14],boxes[15]]];
+
+const upKey =()=>{
+
+}
+const clear = ()=>{
+
+    for(let i = 0 ;i < 16 ;i++){
+        boxes[i].innerHTML = "" ;
+    }
+}
+const random_spawn = ()=>{
+    let empty =[]; 
+    for(let i = 0 ; i< 16 ;i++){
+        if(boxes[i].innerHTML ==="" ){
+            empty.push(i);
+        }
+    }
+    let number = Math.random()*(empty.length)
+    console.log(number) ;   
+}
+window.addEventListener("scroll" ,()=>{
+    random_spawn()
+}) ;
